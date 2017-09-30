d9f3393ebc39340b938970d6893125eee4d0e301
Dylan Taylor <i.djt93@gmail.com>
2017-09-30 09:43:17 -0400

    Add files via upload

---
+++ Start_Server.bat
@@ -0,0 +1,12 @@
+@echo off
+color 20
+title Zoned Out RP Server
+echo -
+echo Zoned Out RP
+echo -
+echo Attempting to change directories
+cd /d D:\ZonedOutRP
+echo Success!
+echo Now executing Zoned Out RP Server
+echo Please wait a second...
+D:\ZonedOutRP\run.cmd +exec server.cfg
